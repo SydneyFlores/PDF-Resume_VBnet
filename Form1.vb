4cf10e1cafc9a88d1e4a1665479d5ad6863575d8
SydneyFlores <mharsydney@gmail.com>
2022-07-23 19:18:04 +0800

    added method named mypdfInfo,get the value of jsonfile

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports Newtonsoft.Json
+Public Class Form1
     Private Sub btnconvert_Click(sender As Object, e As EventArgs) Handles btnconvert.Click
 
     End Sub
+    Public Class mypdfInfo
+        Public Property FullName As String
+        Public Property Age As String
+        Public Property Address As String
+        Public Property ContactNumber As String
+        Public Property EmailAddress As String
+        Public Property Objective As String
+        Public Property Skills As String
+        Public Property WorkExperience As String
+        Public Property Education As String
+        Public Property Certification As String
+        Public Property Signature As String
+    End Class
+
 End Class
