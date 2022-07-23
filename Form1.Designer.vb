01afff23aff2c366b3e72fb56b4d58f74db0c493
SydneyFlores <mharsydney@gmail.com>
2022-07-23 18:52:33 +0800

    pdf resume design created

---
+++ Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnconvert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnconvert
+        '
+        Me.btnconvert.Location = New System.Drawing.Point(106, 234)
+        Me.btnconvert.Name = "btnconvert"
+        Me.btnconvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnconvert.TabIndex = 0
+        Me.btnconvert.Text = "Convert"
+        Me.btnconvert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 315)
+        Me.Controls.Add(Me.btnconvert)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PDF Resume"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnconvert As Button
+End Class
