a7c8489774e5d0f17328b8efb51476aef62a6200
SydneyFlores <mharsydney@gmail.com>
2022-07-23 19:36:32 +0800

    pdfname added successfully

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,21 @@
 ﻿Imports System.IO
 Imports Newtonsoft.Json
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
 Public Class Form1
     Private Sub btnconvert_Click(sender As Object, e As EventArgs) Handles btnconvert.Click
         Dim output As mypdfInfo = JsonConvert.DeserializeObject(Of mypdfInfo)(File.ReadAllText("Resume.json"))
+        Dim mypdfresume As Document = New Document()
+        PdfWriter.GetInstance(mypdfresume, New FileStream("FLORES_MHARSYDNEY.pdf", FileMode.Create))
+        mypdfresume.Open()
+        Dim name As Paragraph = New Paragraph(output.FullName)
+
+        name.Font.Size = 20
+        name.Alignment = Element.ALIGN_CENTER
+
+        mypdfresume.Add(name)
+        mypdfresume.Close()
+        MessageBox.Show("Successfuly")
 
     End Sub
     Public Class mypdfInfo
