c9db591c248d2063a7248c46fecd170ef332d331
SydneyFlores <mharsydney@gmail.com>
2022-07-23 20:08:13 +0800

    pdf objective and skills added successfully

--- Form1.vb
+++ Form1.vb
@@ -11,16 +11,26 @@ Public Class Form1
         Dim name As Paragraph = New Paragraph(output.FullName)
         Dim age As Paragraph = New Paragraph(output.Age)
         Dim address As Paragraph = New Paragraph(output.Address)
+        Dim contactnumber As Paragraph = New Paragraph(output.ContactNumber)
+        Dim emailaddress As Paragraph = New Paragraph(output.EmailAddress & vbLf & vbLf & vbLf)
+        Dim objective As Paragraph = New Paragraph("OBJECTIVE" & vbLf & vbTab & output.Objective)
+        Dim skills As Paragraph = New Paragraph("SKILLS" & vbLf & output.Skills)
 
 
         name.Font.Size = 20
         name.Alignment = Element.ALIGN_CENTER
         age.Alignment = Element.ALIGN_CENTER
         address.Alignment = Element.ALIGN_CENTER
+        contactnumber.Alignment = Element.ALIGN_CENTER
+        emailaddress.Alignment = Element.ALIGN_CENTER
 
         mypdfresume.Add(name)
         mypdfresume.Add(age)
         mypdfresume.Add(address)
+        mypdfresume.Add(contactnumber)
+        mypdfresume.Add(emailaddress)
+        mypdfresume.Add(objective)
+        mypdfresume.Add(skills)
         mypdfresume.Close()
         MessageBox.Show("Successfuly")
 
